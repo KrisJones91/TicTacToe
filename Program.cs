2979858be60a060589dc1b24eca59cb49959152c
KrisJones91 <kristop.jones@gmail.com>
2022-06-27 10:14:55 -0600

    board complete

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ Console.ReadKey();
 
 static void SetField()
 {
-    Console.WriteLine("     |       |      ");
-
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+    Console.WriteLine("_____|_____|_____");
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+    Console.WriteLine("_____|_____|_____");
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+    Console.WriteLine("     |     |     ");
 }
