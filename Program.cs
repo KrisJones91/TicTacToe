194da39733241a214fb13a1ea5d950de27833193
KrisJones91 <kristop.jones@gmail.com>
2022-06-27 10:43:02 -0600

    logic setup for table beginning

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-SetField();
+﻿Program1.SetField();
 Console.ReadKey();
 
-static void SetField()
+public class Program1
 {
-    Console.WriteLine("     |     |     ");
-    Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
-    Console.WriteLine("_____|_____|_____");
-    Console.WriteLine("     |     |     ");
-    Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
-    Console.WriteLine("_____|_____|_____");
-    Console.WriteLine("     |     |     ");
-    Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
-    Console.WriteLine("     |     |     ");
+    static char[,] playField =
+{
+    {'1','2','3'}, //row 0
+    {'4','5','6'}, // row 1
+    {'7','8','9'}, // row 2
+};
+    public static void SetField()
+    {
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0, 0], playField[0, 1], playField[0, 2]);
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1, 0], playField[1, 1], playField[1, 2]);
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);
+        Console.WriteLine("     |     |     ");
+    }
 }
+
